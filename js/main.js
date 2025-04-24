d9fb4cb59093e71763da3ccd8109fb876f7872c8
bradyshehu <shehu.brady@gmail.com>
2025-04-24 16:27:15 +0200

    add responsive and event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const apiUri = "https://lanciweb.github.io/demo/api/pictures/";
 const memoriesEl = document.getElementById("memories");
-const sectionEl = document.querySelector();
 
 axios
 
@@ -11,23 +10,28 @@ axios
     responsesArray.data.forEach((response) => {
       const { id, title, date, url } = response;
       generatedHTml += `
-        <div class="col-4">
-          <div class="section p-3 bg-white text-dark">
-            <div class="section-img">
-              <img src="./img/pin.svg" alt="pin" id="pin" />
-              <img
-                src="${url}"
-                alt="test"
-                width="100%"
-              />
-            </div>
-            <div class="section-text pt-3">
-                <p class="text-muted">${date}</p>
-                <h3>${title}</h3>
-            </div>
+      <div class="col-xl-4 col-md-6 col-sm-12">
+        <div id="card-${id}" class="section p-3 bg-white text-dark">
+          <div class="section-img">
+            <img src="./img/pin.svg" alt="pin" id="pin" />
+            <img
+              src="${url}"
+              alt="test"
+              width="100%"
+            />
+          </div>
+          <div class="section-text pt-3">
+            <p class="text-muted">${date}</p>
+            <h3>${title}</h3>
           </div>
         </div>
-    `;
+      </div>
+      `;
     });
     memoriesEl.innerHTML = generatedHTml;
+    const sectionEl = document.querySelector(".section.p-3.bg-white.text-dark");
+    sectionEl.addEventListener("click", function (e) {
+      e.preventDefault();
+      alert("ciao");
+    });
   });
