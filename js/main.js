a4aa30f7e0746050c2293375815e8882041c02bb
bradyshehu <shehu.brady@gmail.com>
2025-04-24 17:28:22 +0200

    latest fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const apiUri = "https://lanciweb.github.io/demo/api/pictures/";
 const memoriesEl = document.getElementById("memories");
-
+const bigImageEl = document.getElementById("open-image");
+const closeButtonEl = document.getElementById("close-button");
 axios
 
   .get(apiUri)
@@ -18,6 +19,7 @@ axios
               src="${url}"
               alt="test"
               width="100%"
+              class="memory"
             />
           </div>
           <div class="section-text pt-3">
@@ -29,9 +31,27 @@ axios
       `;
     });
     memoriesEl.innerHTML = generatedHTml;
-    const sectionEl = document.querySelector(".section.p-3.bg-white.text-dark");
-    sectionEl.addEventListener("click", function (e) {
-      e.preventDefault();
-      alert("ciao");
-    });
+    const sectionsEl = document.querySelectorAll(
+      ".section.p-3.bg-white.text-dark"
+    );
+
+    // ******************* DA SISTEMARE *************************
+
+    // for (i = 0; i < sectionsEl.length; i++) {
+    //   const section = sectionsEl[i];
+    //   section.addEventListener("click", function (e) {
+    //     const imgEl = document.querySelectorAll(".memory");
+    //     e.preventDefault();
+    //     console.log("ciao");
+    //     for (j = 0; j < imgEl.length; j++) {
+    //       if ((i = j)) {
+    //         section.classList.add("d-none");
+    //         bigImageEl.classList.remove("d-none");
+    //         bigImageEl.append(imgEl[i]);
+    //       }
+    //     }
+    //   });
+    // }
   });
+
+// bigImageEl.append(imgEl)
