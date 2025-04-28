d3bb371fb7da260cb19b1c133f96e9140cfbffa3
bradyshehu <shehu.brady@gmail.com>
2025-04-28 11:27:14 +0200

    fix on event listener

--- js/main.js
+++ js/main.js
@@ -12,15 +12,15 @@ axios
       const { id, title, date, url } = response;
       generatedHTml += `
       <div class="col-xl-4 col-md-6 col-sm-12">
-        <div id="card-${id}" class="section p-3 bg-white text-dark">
+        <div id="section-${id}" class="section p-3 bg-white text-dark">
           <div class="section-img">
-            <img src="./img/pin.svg" alt="pin" id="pin" />
-            <img
-              src="${url}"
-              alt="test"
-              width="100%"
-              class="memory"
-            />
+          <img
+          src="${url}"
+          alt="test"
+          width="100%"
+          class="memory"
+          />
+          <img src="./img/pin.svg" alt="pin" id="pin" />
           </div>
           <div class="section-text pt-3">
             <p class="text-muted">${date}</p>
@@ -37,21 +37,18 @@ axios
 
     // ******************* DA SISTEMARE *************************
 
-    // for (i = 0; i < sectionsEl.length; i++) {
-    //   const section = sectionsEl[i];
-    //   section.addEventListener("click", function (e) {
-    //     const imgEl = document.querySelectorAll(".memory");
-    //     e.preventDefault();
-    //     console.log("ciao");
-    //     for (j = 0; j < imgEl.length; j++) {
-    //       if ((i = j)) {
-    //         section.classList.add("d-none");
-    //         bigImageEl.classList.remove("d-none");
-    //         bigImageEl.append(imgEl[i]);
-    //       }
-    //     }
-    //   });
-    // }
+    sectionsEl.forEach((section) => {
+      section.addEventListener("click", () => {
+        const imageSrc = document.querySelector(`#${section.id} img`);
+        console.log(imageSrc);
+        const currentUrlImage = imageSrc.getAttribute("src");
+        console.log(currentUrlImage);
+
+        const openHtml = `
+        <div class=
+        `;
+      });
+    });
   });
 
 // bigImageEl.append(imgEl)
